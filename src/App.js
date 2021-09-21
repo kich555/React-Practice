e5c9461266c0d3f97076a69290806ce1ce8d4e58
kich555 <hyunjjang199582@gmail.com>
2021-09-21 18:18:32 +0900

    made InputSample

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Hello from './Hello';
 import Wrapper from './wrapper';
 import Counter from './Counter';
+import InputSample from './InputSample';
 
 
 function App() {
     return (
-        <Counter />
+        <InputSample />
     );
 }
 
---
+++ src/InputSample.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function InputSample() {
+    return (
+        <>
+            <input />
+            <button>초기화</button>
+            <div>
+                <b>값: </b>
+            </div>
+        </>
+    )
+}
+
+export default InputSample;
