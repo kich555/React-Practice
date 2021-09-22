d60ae93b718a196463aa61dec8e0a5716cfc61a5
kich555 <hyunjjang199582@gmail.com>
2021-09-22 16:06:58 +0900

    Add an item to Array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,76 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Hello from './Hello';
 import Wrapper from './wrapper';
 import Counter from './Counter';
 import InputSample from './InputSample';
-
+import CreateUser from './CreateUser';
+import UserList from './UserList';
 
 function App() {
-    return (
-        <InputSample />
-    );
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'joan',
+      email: 'public.joan@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'jenson',
+      email: 'jenson@gmail.com',
+    },
+    {
+      id: 3,
+      username: 'kin',
+      email: 'kin@gmail.com',
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    /// ...
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+
+    setUsers([users.concat(user)]);
+
+    setInputs({
+      username: '',
+      email: '',
+    });
+    nextId.current += 1;
+  };
+
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/UserList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function User({ user }) {
+  return (
+    <div>
+      <b>{user.username}</b> <span>({user.email})</span>
+    </div>
+  );
+}
+
+function UserList({ users }) {
+  return (
+    <>
+      {users.map((user) => (
+        <User user={user} key={user.id} />
+      ))}
+    </>
+  );
+}
+
+export default UserList;
